f53dd0df07dd9428bfab74a426780766bfcbd561
BivekHeka <79736842+BivekHeka@users.noreply.github.com>
2026-03-09 21:53:09 +0545

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,6 @@ weather_params = {
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
-# print(weather_data["list"])
 
 will_rain = False
 for hour_data in weather_data["list"]:
